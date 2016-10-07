97f33ef443d73242895e476856a42939b4d9326d
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2016-10-07 02:42:00 -0400

    just started added ignore file

---
+++ migrations/20161007062315-create-burger.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Burgers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      burgerName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      devoured: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Burgers');
+  }
+};
--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,14 @@
-var orm = require("../config/orm.js");
-
-//stores orm in burger 
-var burger = {
-	//list all burgers
-	selectAllBurgers: function(callback){
-		orm.selectAll('burgers', function(res){
-			callback(res);
-		});
-	},
-
-	//insters one burger to database
-	insertBurger: function(value, callback){
-		orm.insertOne('burgers', 'burger_name', value, function(res){
-			callback(res);
-		})
-	},
-	//updates burger from database
-	updateBurger: function(value, condition, callback){
-		
-		orm.updateOne('burgers', value, condition, function(res){
-			callback(res);
-		})
-	}
-}
-
-module.exports = burger;
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define('Burger', {
+    burgerName: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Burger;
+};
